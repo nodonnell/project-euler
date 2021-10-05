591e0d76299ccaea816851af443ccb740a7c52d2
Niall <nod652@gmail.com>
2021-10-05 22:48:09 +0100

    Solved problem 003.

--- solutions/003_largest_prime_factor.js
+++ solutions/003_largest_prime_factor.js
@@ -2,30 +2,93 @@
 //
 // What is the largest prime factor of the number 600851475143?
 
-function getPrimeFactors(number) {
+function getLargestPrimeFactor(number) {
 
-
+    const primeFactors = getPrimeFactors(number);
+    primeFactors.sort( (a, b) => a - b );
+    return primeFactors[primeFactors.length - 1]
 
 }
 
-function getNextPrime(number, max = 100) {
+function getPrimeFactors(number) {
+
+    let firstDenominator = findSmallestPrimeFactor(number);
+    let secondNumerator = number / firstDenominator;
+    let numerators = [number, secondNumerator];
+    let denominators = [firstDenominator];
+
+    while ( multiplyNumbersInArray(denominators) < number ) {
+
+        const nextDenominator = findSmallestPrimeFactor(numerators[numerators.length -1]);
+        denominators.push( nextDenominator );
+
+        const nextNumerator = numerators[numerators.length - 1] / denominators[denominators.length - 1];
+        numerators.push( nextNumerator );
+
+    }
+
+    return denominators;
+
+}
+
+function getNextPrimeFactor(number, primeFactors) {
+
+    let quotients = [number];
+    
+    for ( let i = 0; i < primeFactors.length; i++ ) {
+
+        const nextQuotient = quotients[i]/primeFactors[i];
+        quotients.push(nextQuotient);
+
+    }
+
+}
+
+function findSmallestPrimeFactor(number, smallestPrime = 2) {
+
+    if ( Number.isInteger(number / smallestPrime) ) return smallestPrime;
+    
+    return findSmallestPrimeFactor(number, getNextPrime(smallestPrime));
+
+}
+
+
+function multiplyNumbersInArray(arrayOfNumbers) {
+
+    let result = 1;
+    arrayOfNumbers.forEach(number => {
+        result = result * number;
+    });
+    return result;
+
+}
+
+function getNextPrime(number, max = inputValue) {
 
     while ( number < max ) {
         
-        number += 1;
+        number++;
         if( isPrime(number) ) {
             return number;
         }
 
     }
 
+    return -1;
+
 }
 
 function isPrime(number) {
 
-    for( let i = 2; i <= number; i++ ) {
+    let factors = 0;
 
-        if( number % i === 0 ) {
+    for ( let i = 2; i <= number; i++ ) {
+
+        if ( Number.isInteger(number / i) ) {
+            factors++;
+        } 
+
+        if ( factors > 1 ) {
             return false;
         }
 
@@ -35,4 +98,7 @@ function isPrime(number) {
 
 }
 
-console.log(getNextPrime(3));
+const inputValue = 600851475143;
+
+console.log(getPrimeFactors(inputValue));
+console.log(getLargestPrimeFactor(inputValue));
